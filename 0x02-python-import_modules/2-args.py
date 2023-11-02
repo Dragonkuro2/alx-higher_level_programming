a21111a71cca7b61dd19d9abde474a107a10c14c
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-02 15:55:19 -0700

    the script code of the task number 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,9 @@ if __name__ == "__main__":
     if numarg == 1:
         print(f"{numarg - 1} arguments.")
     else:
-        print(f"{numarg - 1} arguments:")
-    for i in range(1, numarg):
-        print(f"{i}: {sys.argv[i]}")
+        print(f"{numarg - 1} argument", end="")
+        if numarg > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, numarg):
+            print(f"{i}: {sys.argv[i]}")
