67f723fb91d60b8684796896ebb72ad821dcfb90
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-02 15:33:25 -0700

    the script code of the task number 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    name = sys.argv[0]
+    arguments = sys.argv[1:]
+    numarg = len(sys.argv)
+    print(f"{numarg} arguments:")
+    for i in range(1, numarg):
+        print(f"{numarg}: {sys.argv[i]}")
