c3dcb90bcfc996ae57acdc4d6458b7dedd1b231a
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-06 12:50:56 -0800

    the task number 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for iteam in range(0, len(my_list)):
-        print("{}".format(my_list[iteam]))
+    for item in my_list:
+        print("{:d}".format(item))
