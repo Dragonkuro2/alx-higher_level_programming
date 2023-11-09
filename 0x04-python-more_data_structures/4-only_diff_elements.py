e3c2e64f70e6df79e982c038dc723b97ad80df9a
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-08 16:17:13 -0800

    the script code of the task number 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
+    # the ^ is just like if we wrote set1.symmetric_difference(set2)
+    # and it mean just like NAND
     set_3 = set_1 ^ set_2
     return set_3
