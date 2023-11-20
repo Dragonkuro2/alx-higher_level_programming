35a14fc1f48871f663ae21dd5fc88749c6c17aad
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-20 11:31:47 -0800

    the script code of the task number 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 j += 1
             i += 1
         print()
-    except (IndexError, ValueError, TypeError):
+    except (ValueError, TypeError):
         print("list index out of range")
     return j
