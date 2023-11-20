69f92e34a3c0972238a88ee2915c3e6748a16375
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-20 09:10:22 -0800

    the script code of the task number 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        num = 0
         for idx in range(x):
             print(my_list[idx], end="")
-        print()
+            num += 1
     except IndexError:
-        print("the number of element that you enter doesn't exist in the list")
-    except:
-        print("An unknown Error occured")
+        None
+    print()
+    return(num)
