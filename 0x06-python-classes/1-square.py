9a82a311d7f13be8048d65c770d05ef1fb95f32c
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-21 11:23:10 -0800

    the scriptcode of the task number 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''I create this class that named Square that will createa private attribute'''
+
+
+class Sqaure:
+    def __init__(self, size):
+        ''' init function
+            Arguments:
+          --------------
+        size: the size of square '''
+
+        self.__size = size
