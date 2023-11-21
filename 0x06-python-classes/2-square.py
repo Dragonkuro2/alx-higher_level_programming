f9831353bc742c298f2fc622774a18d829b28256
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-21 12:20:47 -0800

    the script code of the task number 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    ''' A class representing a square. '''
+
+    def __init__(self, size=0):
+        ''' initializes a square instance with a specified size.
+
+            Arrguments:
+                size (int): the size of the square. by default it is 0.
+
+            Raises:
+                TypeError: if the size is not an integer.
+                ValueError: if the size is less than 0.
+        '''
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
