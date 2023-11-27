0c6a5b44e338bb6493345cef7be7af8603c1b5bd
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-27 10:54:47 -0800

    the task number 0 and the readme file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" The module of the class Rectangle. """
+
+
+class Rectangle:
+    """ Empty class with the name Rectangle. """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is the readme file of the project '0x08-python-more_classes' you can find here all the tasks that I did from this project
