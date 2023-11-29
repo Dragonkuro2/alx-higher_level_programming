60c380c809358c514a186934634ded2956c519c0
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-11-28 16:02:05 -0800

    the task number 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+""" Module of the class LockedClass. """
+
+
 class LockedClass:
+    """ This class is the lockedclass that contain just __stots__ """
+
+    __slots__ = ["first_name"]
