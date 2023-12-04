770c652957eabfe6d000ec21ee3e270472ef2fa4
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-04 13:40:15 -0800

    the task number 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module for the function lookup. """
+
+
+def lookup(obj):
+    """ This function return a list of available attributes
+        and methods of an object. """
+
+    return dir(obj)
