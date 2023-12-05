07299f2cddff3d807348ac14e995ac7085ed61d0
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-05 01:12:35 -0800

    the task number 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This is the module of the function is_kind_of_class. """
+
+
+def is_kind_of_class(obj, a_class):
+    """ this function check if the object is part of the a_class. """
+    return isinstance(obj, a_class)
