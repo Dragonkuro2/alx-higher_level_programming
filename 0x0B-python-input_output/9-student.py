5c49d7a312f805f59de3fd5c2620fe96b91c8cf6
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-06 14:11:11 -0800

    the task number 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+''' 9-student.py Module. '''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        ''' initialize the public instances first_name, last_name, age. '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        ''' retrieves a dictionary representation of a Student instance. '''
+        return self.__dict__
