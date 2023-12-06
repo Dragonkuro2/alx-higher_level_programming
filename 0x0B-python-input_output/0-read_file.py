0a9b46a877c5fc317b869c802721456d34153805
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-06 02:44:13 -0800

    the task number 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 ''' Module of the file 0-read_file.py that include the function raed_file. '''
 
 
 def read_file(filename=""):
     ''' this function read and print the content of the input file. '''
-    with open(filename, "r", encoding='utf-8') as data:
+    with open(filename, "r") as data:
         print(data.read(), end="")
