9bb4065438bcbfff4cd16bd3733477ec6da80e5d
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-06 15:44:13 -0800

    the task number 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' the file  9-student.py Module. '''
+
+
+class Student:
+    ''' This class has two methodes init and to_json. '''
+    def __init__(self, first_name, last_name, age):
+        ''' initialize the public instances first_name, last_name, age. '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' retrieves a dictionary representation of a Student instance. '''
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        mydict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                mydict[key] = value
+        return mydict
+
+    def reload_from_json(self, json):
+        ''' replaces all attributes of the Student instance. '''
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
