ddd8dfc7113c029a1d29acf96fa10d4c4646d305
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-06 03:36:42 -0800

    the task number 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' thsi module is for the function to_json_string. '''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' this function return the object that returns to string. '''
+    return json.dumps(my_obj)
