a88745ad9fb61ae1f612680ddb1e7e0c50b7c293
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-12-11 15:32:03 -0800

    the task number 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' square.py '''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' square class. '''
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' initialize the size and x, y and the id. '''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        ''' overwrite the str methode. '''
+        return f"[Square] ({self.id}) {self.x}/{self.y} - self.size"
