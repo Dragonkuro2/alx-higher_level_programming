5719f286b19e3fe9dc1f50eae0789616bb9a552c
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-02-14 16:40:52 -0800

    the task number 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 let result = 1;
 const arg = Number(process.argv[2]);
+function fuctorial (number) {
+  if (number == 0) {
+    return (1);
+  } else {
+      return (number * fuctorial(number - 1));
+  }
+}
+
 if (isNaN(arg)) {
   console.log(1);
 } else {
-  for (let i = arg; i > 0; i--) {
-    result *= i;
-  }
-  console.log(result);
+  console.log(fuctorial(arg));
 }
