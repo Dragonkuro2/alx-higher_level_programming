1b31ca0168d91b78d1474107e6dfd6c91ecce644
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-02-17 04:31:44 -0800

    the task number 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const dataA = fs.readFileSync(fileA, { encoding: 'utf8' });
+const dataB = fs.readFileSync(fileB, { encoding: 'utf8' });
+fs.writeFileSync(fileC, dataA + dataB, { encoding: 'utf8' });
