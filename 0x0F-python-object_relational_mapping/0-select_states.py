424416b1c063ffc6b77e6330e91655b30b820566
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-02-18 05:41:14 -0800

    I fixed the error of tyhe checker of pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ from sys import argv
 # with this condion the code will should not work when imported.
 if __name__ == "__main__":
     # making connection to the database
-    connection = MySQLdb.connect(host = "localhost", port = 3306,
-            user = argv[1], passwd = argv[2], db = argv[3])
+    connection = MySQLdb.connect(host="localhost", port=3306,
+                                 user=argv[1], passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
     query = "SELECT * FROM states"
     cursor.execute(query)
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
 
-    #we use the methode close() to clean up the process after they done.
+    # we use the methode close() to clean up the process after they done.
     cursor.close()
     connection.close()
