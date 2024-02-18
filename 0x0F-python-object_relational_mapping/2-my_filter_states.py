a075a3fc7d0990ff29fef99e4f581b97c93c5994
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-02-18 11:31:44 -0800

    In the last code the search wasn't sensitive that's mean that there's no difference between 'A' and 'a', and I fixed that problem

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                                  passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
-    query = "SELECT * FROM states WHERE name LIKE '{}'".format(argv[4])
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
 
