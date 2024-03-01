058c6c28900990eb4b4b7cb64111332bc61d06be
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-03-01 15:24:08 -0800

    task number 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a given URL with a given JSON file.
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -sX POST $1 -H "Content-Type: application/json" -d @$2 -L
