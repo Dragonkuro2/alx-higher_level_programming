018ee8a2d85633839e222ec436b18433ef619c72
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-03-01 13:25:30 -0800

    task number 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response.
-curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -L
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
