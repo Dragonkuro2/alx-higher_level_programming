6501b1c6b885ecfdbe38913223a71fd12c474672
Hicham Bourezi <Hicham.bourezi@gmail.com>
2024-03-01 15:32:36 -0800

    task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response.
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
+curl -o /dev/null -sw "%{http_code}" $1
